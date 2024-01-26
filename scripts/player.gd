7a8a770dc4e229c6ae89f6b2326e76bb4b7ed4c0
Jean-Martin Vlaeminck <jmartin.vlaeminck@gmail.com>
2024-01-27 00:58:06 +0100

    Re-adding the player.gd

---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+signal hit
+
+@export var speed = 400
+var screen_size: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity_x = Input.get_axis("move_left", "move_right")
+	# If the player presses both buttons, he doesn't move I guess?
+	if abs(velocity_x) > 0:
+		velocity_x *= speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	var velocity = Vector2(velocity_x, 0)
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true) # Disable to avoid receiving lots of hit signals
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
