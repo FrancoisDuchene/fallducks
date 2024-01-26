dbe733fe3c9c1b0dee87c6ccedb128ee354e1eab
Francois Duchene <duchene.francois@protonmail.com>
2024-01-27 00:04:16 +0100

    upgrade oisillon + hud

--- scripts/ui/HUD.gd
+++ scripts/ui/HUD.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+# Notifies 'Main' node that the button has been pressed
+signal start_game
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +12,31 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+	
+func show_game_over():
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down
+	await $MessageTimer.timeout
+	
+	$Message.text = "FLY BABY"
+	$Message.show()
+	
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+	
+func _on_message_timer_timeout():
+	$Message.hide()
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
